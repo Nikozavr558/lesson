0c49269f656ceceba14f277884572e2471a7fb8c
Roman <spacewolf1986@yandex.ru>
2024-07-27 13:48:21 +0300

    Исправил неточности урока 1

--- public/working-with-variables.php
+++ public/working-with-variables.php
@@ -1,19 +1,19 @@
 <?php
 $a = 10;
 $b = 20;
-$ammount = $a + $b;
-$Difference = $a - $b;
+$amount = $a + $b;
+$difference = $a - $b;
 $composition = $a * $b;
-$Private = $a / $b;
+$private = $a / $b;
 $remains = $a % $b;
 echo "a = 10 b = 20";
 echo "<br>";
-echo "Сумма a и b = $ammount";
+echo "Сумма a и b = $amount";
 echo "<br>";
-echo "Разность a и b = $Difference";
+echo "Разность a и b = $difference";
 echo "<br>";
 echo "Произведение a и b = $composition";
 echo "<br>";
-echo "Частное a и b = $Private";
+echo "Частное a и b = $private";
 echo "<br>";
 echo "Остаток деления a и b = $remains";
