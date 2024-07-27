fa422decdc3b1e7bec78f11031466a057fccc8fc
Nikozavr558 <160863265+Nikozavr558@users.noreply.github.com>
2024-07-27 16:43:22 +0300

    Delete public/task-3.php

--- public/task-3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$appleNum = 2;
-$orangeNum = 3;
-$sumFruits = $appleNum + $orangeNum;
-$string = "У меня $appleNum яблока и $orangeNum апельсина.";
-$length = strlen($string);
-echo "Длина строки " . $length;
-echo '<br>';
-echo "У меня $sumFruits яблок и $sumFruits апельсинов";
