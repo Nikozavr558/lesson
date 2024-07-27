4f50a82c5fb786e2f68340e287dcfa354944fccc
Roman <spacewolf1986@yandex.ru>
2024-07-27 13:35:27 +0300

    Сделал урок 1

---
+++ public/working-with-variables.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 20;
+$ammount = $a + $b;
+$Difference = $a - $b;
+$composition = $a * $b;
+$Private = $a / $b;
+$remains = $a % $b;
+echo "a = 10 b = 20";
+echo "<br>";
+echo "Сумма a и b = $ammount";
+echo "<br>";
+echo "Разность a и b = $Difference";
+echo "<br>";
+echo "Произведение a и b = $composition";
+echo "<br>";
+echo "Частное a и b = $Private";
+echo "<br>";
+echo "Остаток деления a и b = $remains";
