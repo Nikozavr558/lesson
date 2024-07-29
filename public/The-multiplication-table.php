94aff2a5967434117d4ec622063808e3c574db67
Roman <spacewolf1986@yandex.ru>
2024-07-29 18:31:30 +0300

    Таблица умножения через for, решение.

---
+++ public/The-multiplication-table.php
@@ -0,0 +1,42 @@
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 2 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 3 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 4 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 5 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 6 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 7 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 8 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 9 . " ";
+}
+echo '<br>';
+for ($i = 1; $i <= 10; $i++) {
+    echo $i * 10 . " ";
+}
+echo '<br>';
+
