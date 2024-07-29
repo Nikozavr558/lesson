889a10f9ac3dba78299b5f2f3773e3cc8d7686de
Roman <spacewolf1986@yandex.ru>
2024-07-29 19:10:53 +0300

    fix Таблица умножения через for, решение.

--- public/The-multiplication-table.php
+++ public/The-multiplication-table.php
@@ -1,42 +1,7 @@
 <?php
-for ($i = 1; $i <= 10; $i++) {
-    echo $i . " ";
+for ($j = 1; $j <= 10; $j++) {
+    for ($i = 1; $i <= 10; $i++) {
+        echo $i * $j . " ";
+    }
+    echo '<br>';
 }
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 2 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 3 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 4 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 5 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 6 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 7 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 8 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 9 . " ";
-}
-echo '<br>';
-for ($i = 1; $i <= 10; $i++) {
-    echo $i * 10 . " ";
-}
-echo '<br>';
-
