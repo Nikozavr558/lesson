23e0e6d9feb24071792c23082a9392fe16e42230
Roman <spacewolf1986@yandex.ru>
2024-08-10 13:04:38 +0300

    Roman-php-lesson-4-task-3 feedback form

--- public/process_contact.php
+++ public/process_contact.php
@@ -3,12 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = htmlspecialchars($_POST["username"]);
     $email = $_POST["email"];
     $massage = htmlspecialchars($_POST["message"]);
-    echo "Your massage:";
+    echo "Enter your message:";
     echo "<p><strong>Name:</strong> $username</p>";
     echo "<p><strong>Email:</strong> $email</p>";
     echo "<p><strong>Message:</strong> $massage</p>";
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Your massage:";
+        echo "Enter your message:";
         echo "<p><strong>Name:</strong> $username</p>";
         echo "<p><strong>Email:</strong> $email</p>";
         echo "<p><strong>Message:</strong> $massage</p>";
