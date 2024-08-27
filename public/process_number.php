7f1d2bc3e4d400664ac97db0c336a1f3730624f4
Roman <spacewolf1986@yandex.ru>
2024-08-27 18:51:01 +0300

    Roman-php-lesson-4-task-5 Numeric form fix

--- public/process_number.php
+++ public/process_number.php
@@ -1,21 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $number = (int)$_POST["number"];
-    if ($number % 2 == 0) {
-        echo "<h1>Четные числа от 0 до $number:</h1>";
-        for ($i = 0; $i <= $number; $i += 2) {
-            echo $i . "<br";
-        }
+    $number = $_POST["number"];
+    if (is_numeric($number)) {
 
-    } else {
-        echo "<h1>Нечетные числа от 0 до $number:</h1>";
-        for ($i = 1; $i <= $number; $i += 2) {
-            echo $i . "<br>";
+
+        if ($number % 2 == 0) {
+            echo "<h1>Четные числа от 0 до $number:</h1>";
+            for ($i = 0; $i <= $number; $i += 2) {
+                echo $i . "<br>";
+            }
+        } else {
+            echo "<h1>Нечетные числа от 0 до $number:</h1>";
+            for ($i = 1; $i <= $number; $i += 2) {
+                echo $i . "<br>";
+            }
         }
         echo "<h1>Все числа массива от 0 до $number:</h1>";
         for ($i = 0; $i <= $number; $i++) {
             echo $i . "<br>";
         }
-    }
 
+    } else {
+        echo "Пошел накуй мамкин хацкер";
+    }
 }
