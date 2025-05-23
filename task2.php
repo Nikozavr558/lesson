8758a5ee578bf53d184730518d5b0cbfc0cee388
Vobzhak <vobzhaks@gmail.com>
2025-05-23 17:48:42 +0400

    Добавил доп задание

--- task2.php
+++ task2.php
@@ -3,4 +3,11 @@ $name = "Владимир";
 $surname = "Кичемаев";
 $age = 32;
 echo  "Мое имя $name $surname и мне уже целых $age года.";
+
+echo sprintf(
+    "Мое имя %s моя фамилия %s и мне %d",
+    $name,
+    $surname,
+    $age
+);
 ?>
