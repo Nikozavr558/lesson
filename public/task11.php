62af8416d1f091267c80f8fe879f67cf8690f3bd
Vobzhak <vobzhaks@gmail.com>
2025-05-27 12:29:45 +0400

    Добавил цикл foreach с sprintf

--- public/task11.php
+++ public/task11.php
@@ -1,1 +1,19 @@
 <?php
+
+$products = [
+    ["name" => "Laptop", "price" => 1000, "quantity" => 5],
+    ["name" => "Mouse", "price" => 20, "quantity" => 50],
+    ["name" => "Monitor", "price" => 150, "quantity" => 0],
+    ["name" => "Keyboard", "price" => 50, "quantity" => 30],
+];
+foreach ($products as $product) {
+    if ($product ["quantity"] > 0) {
+        echo sprintf("product: %s price: %s quantity: %s<br><br>",
+        $product ["name"],
+        $product ["price"],
+        $product ["quantity"]
+    );
+
+    }
+}
+?>
