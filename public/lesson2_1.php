559a5a62cf2dbba0c81ecb1b3b5db7a17c6d309f
Termit <emptytermit@gmail.com>
2026-01-21 19:48:59 +0500

    bud fixes

--- public/lesson2_1.php
+++ public/lesson2_1.php
@@ -4,18 +4,21 @@ $a = 5;
 $b = 10;
 
 $sum = $a + $b;  // сложение
-echo "$a + $b = $sum";
+echo "$a + $b = $sum <br>";
 
 $difference = $a - $b; // разность
-echo "$a - $b = $difference";
+echo "$a - $b = $difference <br>";
 
 $multiplication = $a * $b; // умножение
 echo "$a * $b = $multiplication";
+echo "<br>";
 
 $division = $a / $b; // Деление
 echo "$a / $b = $division";
+echo "<br>";
 
 $remains = $a % $b; // Остаток от деления
 echo "$a % $b = $remains";
+echo "<br>";
 
 ?>
