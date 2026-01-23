db160423e892d14b814bdf16e10bf016d4038891
Termit <emptytermit@gmail.com>
2026-01-23 19:40:06 +0500

    complited_lesson4_1

---
+++ public/Lesson 4/Lesson4_1.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="vievport" content="width=device-width, initial-scale=1.0">
+    <title>POISK</title></head>
+<body>
+    <form action="/search.php" method="GET"
+        <label for="name" >Search:</label>
+        <input type="text" id="name" name="search">
+        <input type="submit" value="Poisk">
+</form>
+</body>
+</html>
+
+
+
+<?php
