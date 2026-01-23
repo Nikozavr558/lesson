747489fd5e7a7439676a193e61e3328702fd730a
Termit <emptytermit@gmail.com>
2026-01-23 16:27:34 +0500

    completing the lesson3_1_9.php

---
+++ public/lesson3_1_10.php
@@ -0,0 +1,14 @@
+<?php
+
+$value = 42.5;
+
+$type = match(true) {
+    is_int($value) => "Число",
+    is_float($value) => "Дробное число",
+    is_string($value) => "Строка",
+    is_array($value) => "Массив",
+    is_bool($value) => "Булевое число",
+    is_null($value) => "ноль",
+};
+
+echo "$value - $type";
