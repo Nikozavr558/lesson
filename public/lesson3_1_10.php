6160c71480a173c12b09c8c1aedbc5877a775a6f
Termit <emptytermit@gmail.com>
2026-01-23 17:38:42 +0500

    completing the lesson3_1_10.php fix

--- public/lesson3_1_10.php
+++ public/lesson3_1_10.php
@@ -9,6 +9,7 @@ $type = match(true) {
     is_array($value) => "Массив",
     is_bool($value) => "Булевое число",
     is_null($value) => "ноль",
+    default => "не существует такого"
 };
 
 echo "$value - $type";
