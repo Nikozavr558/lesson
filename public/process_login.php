02b458401260e089d7e2296cee1332f9aed0408e
Termit <emptytermit@gmail.com>
2026-01-25 00:18:02 +0500

    complited_lesson4_3

--- public/process_login.php
+++ public/process_login.php
@@ -1,7 +1,11 @@
+
 <?php
 
-$login = $_POST['login'];
-echo "Login: $login <br>";
-
-$pass = $_POST['password'];
-echo "Password: $pass <br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $text = $_POST["text"];
+    echo "<p>Name: " . htmlspecialchars($name) . "</p>";
+    echo "<p>Email: " . htmlspecialchars($email) . "</p>";
+    echo "<p>Текст сообщения: " . htmlspecialchars($text) . "</p>";
+}
