5048288874ce189ffb7ae2c5c3de9db8c3ef8349
Termit <emptytermit@gmail.com>
2026-01-26 20:51:36 +0500

    complited_lesson5_2 - v2

--- public/Lesson 5/Lesson5_2.php
+++ public/Lesson 5/Lesson5_2.php
@@ -1,14 +1,15 @@
 <?php
-$name = "mr. Frodo";
-function greetWithTime($name)
-{ $currentHour = date('H');
+function greetWithTime($name) {
+    $currentHour = date('H');
     if ($currentHour >= 6 && $currentHour <12) {
         $text = "Good morning";
-    } elseif ($currentHour >= 12 && $currentHour <18) {
-        $text = "Good day";
     }elseif ($currentHour >= 12 && $currentHour <18) {
+        $text = "Good day";
+    }elseif ($currentHour >= 18 && $currentHour <24) {
         $text = "Good evening";
     } else { $text = "Good nigth";
 } echo $text . ", " . $name . "!";
 }
+
 greetWithTime("mr. Frodo");
+
